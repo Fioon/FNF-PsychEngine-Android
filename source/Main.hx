84a558b92e8388eb66930b00c158ab483845edd6
Fioon <135500399+Fioon@users.noreply.github.com>
2024-03-09 21:29:40 +0800

    kkk

--- source/Main.hx
+++ source/Main.hx
@@ -86,7 +86,8 @@ class Main extends Sprite
 		zoom = 1;
 		
 		ClientPrefs.loadDefaultKeys();
-                addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, updateFramerate, framerate, skipSplash));
+                //addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, updateFramerate, framerate, skipSplash));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
