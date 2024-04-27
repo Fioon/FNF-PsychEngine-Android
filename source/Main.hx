b5981d361d392d7560ecbb0b4961ff7010febb48
Fioon <135500399+Fioon@users.noreply.github.com>
2024-04-27 20:41:07 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -15,7 +15,7 @@ import lime.app.Application;
 import openfl.events.UncaughtErrorEvent;
 import haxe.CallStack;
 import haxe.io.Path;
-import Discord.DiscordClient;
+//import Discord.DiscordClient;
 import sys.FileSystem;
 import sys.io.File;
 import sys.io.Process;
